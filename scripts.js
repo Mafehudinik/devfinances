dfab23da1bf2734db7db7a13b6f0b5fe2b54f5d6
Maria Fernanda Hudinik Alves da Silva <53884339+Mafehudinik@users.noreply.github.com>
2021-06-10 13:59:14 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -126,9 +126,9 @@ const DOM = {
 
 const Uteis = {
     formatAmount(value) {
-        value = Number(value) * 100
+        value = value * 100
         
-        return value
+        return Math.round(value)
     },
 
     formatDate(date) {
